c4abbd54ba6506a601adb2ea58ea6a95f1ee2446
Alexander Apanasovich <alexander.apanasovich@gmail.com>
2017-05-03 14:30:24 +0800

    test

---
+++ src/main/java/servlet/HelloServlet.kt
@@ -0,0 +1,20 @@
+package servlet
+
+import java.io.IOException
+import javax.servlet.ServletException
+import javax.servlet.annotation.WebServlet
+import javax.servlet.http.HttpServlet
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@WebServlet(name = "MyServlet", urlPatterns = arrayOf("/hello"))
+class HelloServlet : HttpServlet() {
+
+    @Throws(ServletException::class, IOException::class)
+    override fun doGet(req: HttpServletRequest, resp: HttpServletResponse) {
+        val out = resp.outputStream
+        out.write("Hello, ${req.getParameter("name")}!".toByteArray())
+        out.flush()
+        out.close()
+    }
+}
