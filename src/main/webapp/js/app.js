a83b9a39c5975a9c544ef2b30807c3dc0349ae10
Alexander Apanasovich <alexander.apanasovich@gmail.com>
2017-05-09 23:25:15 +0300

    z

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -22,13 +22,38 @@ class TaskList extends React.Component {
                     <button
                         style={{marginLeft: "8px"}}
                         className="btn btn-primary float-right"
-                        onClick={this.props.openCreateNewTaskForm}>
+                        data-toggle="modal" data-target="#taskCreateFormModal"
+                        // onClick={this.props.openCreateNewTaskForm}
+                        >
                         <span className="glyphicon glyphicon-plus"/>
                     </button>
                 </div>
                 {this.state.tasks.map(task => (
                     <Task task={task}/>
                 ))}
+            </div>
+        );
+    }
+}
+
+class TaskCreateForm extends React.Component {
+    render() {
+        return (
+            <div id="taskCreateFormModal" className="modal fade" role="dialog">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <button type="button" className="close" data-dismiss="modal">&times;</button>
+                            <h4 className="modal-title">Modal Header</h4>
+                        </div>
+                        <div className="modal-body">
+                            <p>Some text in the modal.</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -85,6 +110,7 @@ class App extends React.Component {
         return (
             <div>
                 <TaskList openCreateNewTaskForm={this.openCreateNewTaskForm}/>
+                <TaskCreateForm />
                 {/*<form onSubmit={this.submitClick}>*/}
                 {/*<label>Кто ты?<br/>*/}
                 {/*<input type="text"*/}
