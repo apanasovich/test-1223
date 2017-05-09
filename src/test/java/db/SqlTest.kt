796cde6168222379269949f7bf5b6ef4265c6dc7
Alexander Apanasovich <alexander.apanasovich@gmail.com>
2017-05-09 09:46:07 +0300

    tags fixed

--- src/test/java/db/SqlTest.kt
+++ src/test/java/db/SqlTest.kt
@@ -8,15 +8,14 @@ class SqlTest : DbTest() {
     fun testBatch() {
         conn.batch("db/1/up.sql")
 
-        conn.update("INSERT INTO TASKS.TASKS (ID, SUMMARY, DESCRIPTION) VALUES (?, ?, ?)",
-                "TSK-01",
+        conn.update("INSERT INTO TASKS.TASKS (SUMMARY, DESCRIPTION) VALUES (?, ?)",
                 "Some Task",
                 "Do this and that"
         )
 
         val list = conn.select("SELECT * FROM TASKS.TASKS")
         assertEquals(1, list.size)
-        assertEquals("TSK-01", list[0]["ID"])
+        assertEquals(1, list[0]["ID"])
         assertEquals("Some Task", list[0]["SUMMARY"])
         assertEquals("Do this and that", list[0]["DESCRIPTION"])
     }
