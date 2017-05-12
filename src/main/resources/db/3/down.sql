7041afb638bd07571e04b4ab84114fe357fff398
Alexander Apanasovich <alexander.apanasovich@gmail.com>
2017-05-12 09:49:01 +0300

    z

--- src/main/resources/db/3/down.sql
+++ src/main/resources/db/3/down.sql
@@ -1,4 +1,4 @@
-alter table TASKS.TASKS alter column SUMMARY column type VARCHAR(100)
+alter table TASKS.TASKS alter column SUMMARY type VARCHAR(100)
 /
 alter table TASKS.TASKS alter column DESCRIPTION type VARCHAR(100)
 /
